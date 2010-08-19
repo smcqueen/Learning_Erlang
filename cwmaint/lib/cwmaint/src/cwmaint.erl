d3485d7f30ca888e779123cd7aca3121b7cea2f1
Stan McQueen <smcqueen@contentwatch.com>
2010-08-19 16:41:53 -0600

    Changes

--- cwmaint/lib/cwmaint/src/cwmaint.erl
+++ cwmaint/lib/cwmaint/src/cwmaint.erl
@@ -1,6 +1,6 @@
 -module(cwmaint).
 
--export([do_maintenance/0]).
+-export([start/0]).
 
 -record(state, {idlist}).
 
@@ -10,24 +10,31 @@
 mylog(_A, _B, _C, _D) ->
     ok.
 
+start() ->
+    application:start(sasl),
+    application:start(mnesia),
+    application:start(resource_discovery),
+    application:start(simple_cache),
+    do_maintenance().
+
 do_maintenance() ->
-    State = #state{},
-    Now = calendar:local_time(),
     mysql:start_link(db, "dbserver", "masteruser", "mast3rus3r", "prorelay"),
     {data, Result} = mysql:fetch(db, "select orgid from companies"),
-    Tablelist = mysql:get_result_rows(Result),
-    io:format("Got ~p orgids~n", [length(Tablelist)]),
-    process_tables(Now, Tablelist, State).
+    Orglist = mysql:get_result_rows(Result),
+    io:format("Got ~p orgids~n", [length(Orglist)]),
+    State = #state{},
+    find_tables(Orglist, State),
+    process_tables().
 
-process_tables(_Now, [], _State) ->
+find_tables([], _State) ->
     ok;
-process_tables(Now, List, State) ->
-    [H|T] = List,
+find_tables(Orglist, State) ->
+    [H|T] = Orglist,
     [Orgid|_] = H,
     Table = "activity" ++ integer_to_list(Orgid),
-    Select = "select activityid, createdatetime from " ++ Table ++ " order by createdatetime desc limit " ++ ?MAX,
+    Select = "select count(*) from " ++ Table,
     case (mysql:fetch(db, Select)) of
-	{data, MysqlRes} ->
+	{data, _MysqlRes} ->
 	    Idlist=State#state.idlist,
 	    case Idlist of
 		undefined ->
@@ -35,16 +42,49 @@ process_tables(Now, List, State) ->
 		_ ->
 		    NewState = State#state{idlist=[Orgid | Idlist]}
 	    end,
-	    io:format("~p~n", [NewState]),
-	    AllRows = mysql:get_result_rows(MysqlRes),
-	    process_rows(Now, AllRows, Table);
+	    simple_cache:insert(orgids, NewState#state.idlist),
+	    io:format("~p~n", [NewState]);
 	{error, _Error} ->
 	    NewState = State,
 	    ok
     end,
-    process_tables(Now, T, NewState).
+    find_tables(T, NewState).
 
-process_rows(_Now, [], Table) ->
+process_tables() ->
+    case simple_cache:lookup(orgids) of
+	{ok, undefined} ->
+	    {ok, no_tables};
+	{ok, Orgidlist} ->
+	    Now = calendar:local_time(),
+	    case length(Orgidlist) of
+		1 ->
+		    Orgid = lists:flatten(Orgidlist),
+		    simple_cache:insert(orgids, undefined);
+		_ ->
+		    [Orgid | T] = Orgidlist,
+		    simple_cache:insert(orgids, T)
+	    end,
+	    process_tables(Now, Orgid);
+	{error, not_found} ->
+	    {error, no_tables}
+    end.
+
+process_tables(_Now, []) ->
+    ok;
+process_tables(Now, Orgidlist) ->
+    [Orgid|T] = Orgidlist,
+    Table = "activity" ++ integer_to_list(Orgid),
+    Select = "select activityid, createdatetime from " ++ Table ++ " order by createdatetime desc limit " ++ ?MAX,
+    case (mysql:fetch(db, Select)) of
+	{data, MysqlRes} ->
+	    AllRows = mysql:get_result_rows(MysqlRes),
+	    process_rows(Now, AllRows, Table);
+	{error, _Error} ->
+	    ok
+    end,
+    process_tables(Now, T).
+    
+process_rows(_Now, [], _Table) ->
     ok;
 process_rows(Now, AllRows, Table) ->
     [H|T] = AllRows,
