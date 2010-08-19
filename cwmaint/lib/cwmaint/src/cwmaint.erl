78b479647505d8139b2cd9f5a96b5fa3aa793fa7
Stan McQueen <stan.mcqueen@gmail.com>
2010-08-19 07:58:33 -0600

    In process

--- cwmaint/lib/cwmaint/src/cwmaint.erl
+++ cwmaint/lib/cwmaint/src/cwmaint.erl
@@ -2,6 +2,8 @@
 
 -export([do_maintenance/0]).
 
+-record(state, {tablename, idlist}).
+
 mylog(_A, _B, _C, _D) ->
     ok.
 
@@ -11,9 +13,10 @@ do_maintenance() ->
 % [2,{datetime,{{2007,10,24},{14,6,7}}}],
 % [3,{datetime,{{2007,10,24},{14,6,9}}}]],
 %    process_rows(Now, AllRows).
-    mysql:start_link(db, "dbserver", "masteruser", "mast3rus3r", "relay", mylog),
+    mysql:start_link(db, "dbserver", "masteruser", "mast3rus3r", "relay"),
     {data, Result} = mysql:fetch(db, "select orgid from companies"),
     Tablelist = mysql:get_result_rows(Result),
+    io:format("Got ~p orgids~n", [length(Tablelist)]),
     process_tables(Now, Tablelist).
 
 process_tables(_Now, []) ->
@@ -22,34 +25,37 @@ process_tables(Now, List) ->
     [H|T] = List,
     [Orgid|_] = H,
     Table = "activity" ++ integer_to_list(Orgid),
-    Select = "select activityid, createdatetime from " ++ Table,
+    Select = "select activityid, createdatetime from " ++ Table ++ " order by createdatetime desc limit 1000",
     case (mysql:fetch(db, Select)) of
 	{data, MysqlRes} ->
 %	    io:format("~p~n", [Result]),
 	    AllRows = mysql:get_result_rows(MysqlRes),
 %	    io:format("~p~n", [AllRows]),
-	    process_rows(Now,AllRows),
+	    io:format("Table ~p~n", [Table]),
+	    #state{tablename = Table, idlist = []},
+	    io:format("~p~n", [#state{}]),
+	    process_rows(Now,AllRows,Table),
 	    process_tables(Now, T);
 	{error, _Error} ->
 %	    io:format("~p~n", [Error]),
 	    process_tables(Now, T)
     end.
 
-process_rows(_Now, []) ->
+process_rows(_Now, [], Table) ->
     ok;
-process_rows(Now, AllRows) ->
+process_rows(Now, AllRows, Table) ->
     [H|T] = AllRows,
     [Activityid|D] = H,
     [D1|_] = D,
     {datetime, Datetime} = D1,
     Age = age(Now, Datetime),
-    case Age > 30 of
+    case Age > 5 of
 	true ->
 	    io:format("Activityid = ~p, DateTime = ~p", [Activityid, Datetime]),
 	    io:format(", Age = ~p days~n", [Age]),
-	    process_rows(Now, T);
+	    process_rows(Now, T, Table);
 	false ->
-	    process_rows(Now, T)
+	    process_rows(Now, T, Table)
     end.
 
 age(Now, Then) ->
